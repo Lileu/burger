5e554d4c53f47db566cffdf6517eea22c633f38a
Lisa Leu <lis.leu@gmail.com>
2019-09-10 21:27:44 +1000

    added initial files and sql code

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ("cheeseburger", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("golden-axe", true);
+INSERT INTO burgers (burger_name, devoured) VALUES ("double-dragon", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("big mac", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("whoppper", true);
+INSERT INTO burgers (burger_name, devoured) VALUES ("zinger burger", true);
